22120ea92f0f06e2f5351a7b451117fb175b762f
itamarq <itamarq@gmail.com>
2021-11-15 11:48:21 +0200

    itamar algorithm v2.2

--- my_algo_itamar.py
+++ my_algo_itamar.py
@@ -8,6 +8,9 @@ import pandas as pd
 
 
 class MyAlgoItamar:
+    """
+    this method initiates the algorithm from the building json file, calls csv file and output csv file
+    """
 
     def __init__(self, building_json, calls_csv, output_csv):
         self.building = Building(0, 0, [])
@@ -25,18 +28,9 @@ class MyAlgoItamar:
             self.elevators_src.append("0")
             self.elevators_start_time.append("0")
 
-    def initiate_calls_from_csv(self):
-        open_file = open(self.calls_csv)
-        read_file = reader(open_file)
-        calls_data = list(read_file)
-
-        for c in calls_data:
-            time = float(c[1])
-            src = int(c[2])
-            dest = int(c[3])
-            type = c[4]
-            allocatedTo = int(c[5])
-            self.calls_list.append(CallForElevator(time, src, dest, type, allocatedTo))
+    """
+    this method allocated the best elevator for a specific call
+    """
 
     def allocate_elev_to_call(self, src, dest, call_time):
         best_elev_index = 0
@@ -67,8 +61,9 @@ class MyAlgoItamar:
                         return best_elev_index
                 else:
                     elev_speed = float(best_elev.speed)
-                    time_from_elev_dest_to_call_src = dist_from_elev_dest_to_call_src/elev_speed
-                    arrival_time_to_call_src = float(self.elevators_finish_time[best_elev_index]) + time_from_elev_dest_to_call_src
+                    time_from_elev_dest_to_call_src = dist_from_elev_dest_to_call_src / elev_speed
+                    arrival_time_to_call_src = float(
+                        self.elevators_finish_time[best_elev_index]) + time_from_elev_dest_to_call_src
                     self.elevators_start_time[best_elev_index] = arrival_time_to_call_src
                     self.elevators_src[best_elev_index] = src
                     self.elevators_finish_time[best_elev_index] = min_time
@@ -87,7 +82,8 @@ class MyAlgoItamar:
                 else:
                     elev_speed = float(best_elev.speed)
                     time_from_elev_dest_to_call_src = dist_from_elev_dest_to_call_src / elev_speed
-                    arrival_time_to_call_src = float(self.elevators_finish_time[best_elev_index]) + time_from_elev_dest_to_call_src
+                    arrival_time_to_call_src = float(
+                        self.elevators_finish_time[best_elev_index]) + time_from_elev_dest_to_call_src
                     self.elevators_start_time[best_elev_index] = arrival_time_to_call_src
                     self.elevators_src[best_elev_index] = src
                     self.elevators_finish_time[best_elev_index] = min_time
@@ -97,12 +93,19 @@ class MyAlgoItamar:
         else:
             elev_speed = float(best_elev.speed)
             time_from_elev_dest_to_call_src = dist_from_elev_dest_to_call_src / elev_speed
-            arrival_time_to_call_src = float(self.elevators_finish_time[best_elev_index]) + time_from_elev_dest_to_call_src
+            arrival_time_to_call_src = float(
+                self.elevators_finish_time[best_elev_index]) + time_from_elev_dest_to_call_src
             self.elevators_start_time[best_elev_index] = arrival_time_to_call_src
             self.elevators_src[best_elev_index] = src
             self.elevators_finish_time[best_elev_index] = min_time
             self.elevators_dest[best_elev_index] = dest
             return best_elev_index
+
+    """
+    this method receives an index of and elevator in the elevators array, call's src and call's destination, than
+    calculates the time for the elevator to arrive to the call's destination taking in consideration the current
+    source and destination of the elevator
+    """
 
     def calculate_finish_time(self, index, src, dest, call_time):
         current_elevator = self.building.elevators[index]
@@ -151,6 +154,11 @@ class MyAlgoItamar:
             expected_finish_time = float(current_finish_time) + total_stop_time + total_travel_time
             return float(expected_finish_time)
 
+    """
+    this methoed returns the current position of the elevator in the elevators array by the given
+     index and by the time given
+    """
+
     def get_current_positition(self, index, time):
         elev_src_floor = self.elevators_src[index]
         elev_speed = float(self.building.elevators[index].speed)
@@ -162,6 +170,11 @@ class MyAlgoItamar:
         else:
             current_position = int(elev_src_floor) - int(elev_speed * time_passed)
         return current_position
+
+    """
+    this method return a boolean value weather a floor is a possible stop, given the elevator index in the elevators
+    array and given the floor and the time we want to be at that floor.
+    """
 
     def is_a_possible_stop(self, index, src, time):
         current_elev_position = self.get_current_positition(index, time)
@@ -178,11 +191,21 @@ class MyAlgoItamar:
                 return False
 
 
+"""
+this method calculated the distance between two floors
+"""
+
+
 def dist(floor_a, floor_b):
     a = int(floor_a)
     b = int(floor_b)
     ans = abs(a - b)
     return ans
+
+
+"""
+this method return the direction of the ride given the source floor and the destination floor
+"""
 
 
 def get_direction(floor_src, floor_dest):
